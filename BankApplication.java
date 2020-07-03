06810b97ebc0fd7b8c1bd6c555ef55b6c3607a18
engrceey <ogbondachristian@gmail.com>
2020-07-03 22:53:14 +0100

    added methods for transactions

--- BankApplication.java
+++ BankApplication.java
@@ -12,3 +12,27 @@ public class BankingApplication{
       customerName = cusName;
       customerId = cusId;
     }
+
+     void deposit(int amount){
+      if(amount != 0){
+        balance += amount;
+        previousTransaction = amount;
+      }
+    }
+
+     void witdraw(int amount){
+        if(amount != 0){
+          balance -= amount;
+          previousTransaction = -amount;
+        }
+      }
+
+    void getLastTransaction(){
+      if(previousTransaction > 0){
+        System.out.println("You Deposited: "+previousTransaction);
+      }else if(previousTransaction < 0){
+        System.out.println("You withdrew: "+ Math.abs(previousTransaction));
+      }else{
+        System.out.println("No Transaction yet");
+      }
+    }
